39c232f1b8e364cbde9b55f9ca339a906cd2432a
RicardodeOliveiraAlecrim <134964500+RicardodeOliveiraAlecrim@users.noreply.github.com>
2023-07-03 19:34:10 +0000

    upgrade value

---
+++ script.js
@@ -0,0 +1,12 @@
+ const confirmedButton = document.querySelector(".confirm-button")
+
+const orderFood = document.querySelector (".select-food")
+
+const drinks = document.queryselector (".select-drinks")
+
+const extras = document.queryselector (".select-extras")
+
+const pay = document.queryselector (".type-payment")
+
+const delivery = document.queryselector (".type-delivery")
+
