fd75e201422d61f19a007485fac45cc66e86e4ca
RicardodeOliveiraAlecrim <134964500+RicardodeOliveiraAlecrim@users.noreply.github.com>
2023-07-04 11:12:02 +0000

    building javascript

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
- const confirmedButton = document.querySelector(".confirm-button")
+const confirmedButton = document.querySelector(".confirm-button")
+
+
+function finishOrder() {
 
 const orderFood = document.querySelector (".select-food")
 
@@ -10,3 +13,15 @@ const pay = document.queryselector (".type-payment")
 
 const delivery = document.queryselector (".type-delivery")
 
+const moneyPay  = document.querySelector(".money")
+
+console.log (".money".value)
+console.log (".type-delivery".value)
+console.log (".type-payment".value)
+console.log (".select-extras".value)
+console.log (".select-drinks".value)
+console.log (".select-food".value)
+  
+}
+
+confirmedButton.addEventListener("click", finishOrder)
